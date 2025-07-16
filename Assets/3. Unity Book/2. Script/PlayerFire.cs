d2d432100edd4439e48f94dec7c8abdfab599292
GitJaeHyeon <wogusworksp@gmail.com>
2025-07-16 17:07:29 +0900

    Update PlayerFire.cs

--- Assets/3. Unity Book/2. Script/PlayerFire.cs
+++ Assets/3. Unity Book/2. Script/PlayerFire.cs
@@ -7,7 +7,7 @@ public class PlayerFire : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire"))
+        if (Input.GetButtonDown("Fire1"))
         {
             GameObject bullet = Instantiate(bulletFactory);
             bullet.transform.position = firePosition.transform.position; // 위치 초기화
