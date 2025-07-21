00700daf4aaca9e1529bf5e56428aa411d8a220a
이재현 <142362487+reappearancee@users.noreply.github.com>
2025-07-21 16:11:08 +0900

    7월 21일 대여장비 -> 맥북

--- Assets/3. Unity Book/2. Script/2D Shooter/Singleton.cs
+++ Assets/3. Unity Book/2. Script/2D Shooter/Singleton.cs
@@ -29,7 +29,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         if (instance == null)
         {
             instance = this as T;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
--- Assets/3. Unity Book/2. Script/3D FPS/FPSPlayerMove.cs
+++ Assets/3. Unity Book/2. Script/3D FPS/FPSPlayerMove.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSPlayerMove : MonoBehaviour
 {
@@ -13,12 +15,28 @@ public class FPSPlayerMove : MonoBehaviour
     public float jumpPower = 10f;
     public bool isJumping = false;
 
+    public int hp = 20;
+    
+    private int maxHp = 20;
+    public Slider hpSlider;
+
+    public GameObject hitEffect;
+
+    public float JumpPower
+    {
+        get => jumpPower;
+        set => jumpPower = value;
+    }
+
     void Start()
     {
         cc = GetComponent<CharacterController>();
     }
     void Update()
     {
+        if(FPSGameManager.Instance.gState != FPSGameManager.GameState.Run)
+            return;
+        
         float h =  Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         
@@ -46,5 +64,25 @@ public class FPSPlayerMove : MonoBehaviour
             isJumping = true;
             yVelocity = jumpPower;
         }
+        hpSlider.value = (float)hp / (float)maxHp;
+    }
+
+    public void DamageAction(int damage)
+    {
+        hp -= damage;
+        
+        hpSlider.value = (float)hp / (float)maxHp;
+
+        if (hp > 0)
+        {
+            StartCoroutine(PlayerHitEffect());
+        }
+    }
+
+    IEnumerator PlayerHitEffect()
+    {
+        hitEffect.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        hitEffect.SetActive(false);
     }
 }
