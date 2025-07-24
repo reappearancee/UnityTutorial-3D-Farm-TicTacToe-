6f967996422b73acefa0a4aca48009b0ca340524
이재현 <142362487+reappearancee@users.noreply.github.com>
2025-07-24 11:51:33 +0900

    점심

--- Assets/3. Unity Book/02. Scripts/Path Finding/AgentController.cs
+++ Assets/3. Unity Book/02. Scripts/Path Finding/AgentController.cs
@@ -3,33 +3,26 @@ using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
-    private Transform player;
     private NavMeshAgent agent;
-    
     public Transform[] points;
-    public int index;
-    
+    private int index;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        SetRandomPoint();
+        agent.SetDestination(points[index].position);
     }
 
     void Update()
     {
-        if (agent.remainingDistance <= 1.5f) // 목적지와의 거리가 1.5 이하일 경우
+        if (Vector3.Distance(transform.position, points[index].position) < 1.5f)
         {
-            Debug.Log("목적지 변경");
-            SetRandomPoint();
+            index++;
+            if (index >= points.Length)
+            {
+                index = 0;
+                agent.SetDestination(points[index].position);
+            }
         }
-    }
-
-    private void SetRandomPoint()
-    {
-        int temp = index;
-        while (temp == index)
-            index = Random.Range(0, points.Length);
-        
-        agent.SetDestination(points[index].position);
     }
 }
